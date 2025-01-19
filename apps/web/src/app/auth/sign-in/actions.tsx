7917e1ec90a30ea9f26fc7c542eebafc6e2d7907
eusamir <samirandradee@gmail.com>
2025-01-18 21:21:06 -0300

    feat: actions para ciração de token e redirect para raiz

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,57 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail adress.' }),
+  password: z.string().min(1, { message: 'Please, provide your password.' }),
+})
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const erros = result.error.flatten().fieldErrors
+
+    return { sucess: false, message: null, erros }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    const setCookie = await cookies()
+
+    setCookie.set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { sucess: false, message, erros: null }
+    }
+
+    console.log(err)
+
+    return {
+      sucess: false,
+      message: 'Unexpected error, please try again in a few minutes.',
+      erros: null,
+    }
+  }
+
+  redirect('/')
+}
